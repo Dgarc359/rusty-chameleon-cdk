228e8f83bfc696f3be93f632697cd77f045c80b5
dgarc359 <dgarc359@outlook.com>
2022-06-12 11:30:58 +0000

    remove dotenv package

--- src/chameleon/src/main.rs
+++ src/chameleon/src/main.rs
@@ -1,12 +1,12 @@
 use lambda_http::{run, service_fn, Error, IntoResponse, Request, RequestExt, Response, Body};
 use dryoc::{classic::crypto_sign::crypto_sign_verify_detached};
-use dotenv::dotenv;
+// use dotenv::dotenv;
+use std::env;
 use std::fmt::Write;
 
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/lambda-http/examples
 async fn function_handler(event: Request) -> Result<impl IntoResponse, Error> {
 
-    dotenv().ok();
     let signature = event.headers().get("X-Signature-Ed25519").unwrap();
     let sig_bytes: [u8; 64] =  signature.as_bytes().try_into().unwrap();
 
@@ -17,7 +17,7 @@ async fn function_handler(event: Request) -> Result<impl IntoResponse, Error> {
     write!(&mut timestamp, "{:?}", event.body()).unwrap();
     let message = timestamp.into_bytes();
 
-    let public_key = dotenv::var("PUBLIC_KEY").unwrap();
+    let public_key = env::var("PUBLIC_KEY").unwrap();
     let pub_key_bytes: [u8; 32] = public_key.as_bytes().try_into().unwrap();
 
     // let token = env token
