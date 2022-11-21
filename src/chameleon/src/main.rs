ed35ef8a87d616857f290764c804f3a53207f08d
David Garcia <87497958+Dgarc359@users.noreply.github.com>
2022-11-20 22:05:07 -0500

    add ok

--- src/chameleon/src/main.rs
+++ src/chameleon/src/main.rs
@@ -92,12 +92,12 @@ async fn function_handler(event: Request) -> Result<impl IntoResponse, Error> {
 													kind: 4,
 													data: ResponseData { content: "bar".to_owned() }
 												};
-												Response::builder()
+												Ok(Response::builder()
 														.status(200)
 														.header("content-type", "application/json")
 														// .body("{ \"type\": 1 }".to_string())
 														.body(serde_json::to_string(&res).unwrap())
-														.map_err(Box::new)?
+														.map_err(Box::new)?)
 										}
 
 										// methods for different types of application commands
